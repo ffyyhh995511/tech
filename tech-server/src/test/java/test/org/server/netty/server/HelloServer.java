9e1f421656d4d327a8cafa27e0c22e692ad905ba
方云鹤 <fangyunhe@mingbikes.com>
2017-05-18 20:25:00 +0800

    netty

---
+++ tech-server/src/test/java/test/org/server/netty/server/HelloServer.java
@@ -0,0 +1,37 @@
+package test.org.server.netty.server;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import test.org.server.netty.handle.HelloServerInitializer;
+
+public class HelloServer {
+    
+    /**
+     * 服务端监听的端口地址
+     */
+    private static final int portNumber = 7878;
+    
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup);
+            b.channel(NioServerSocketChannel.class);
+            b.childHandler(new HelloServerInitializer());
+
+            // 服务器绑定端口监听
+            ChannelFuture f = b.bind(portNumber).sync();
+            // 监听服务器关闭监听
+            System.out.println("server init...");
+            f.channel().closeFuture().sync();
+            // 可以简写为
+            /* b.bind(portNumber).sync().channel().closeFuture().sync(); */
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
