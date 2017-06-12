30b4b3e8af1e90c17b8ff5117688fa697dc1de5c
ffyyhh995511 <ffyyhh995511@gmail.com>
2017-06-12 21:33:51 +0800

    test error

---
+++ tech-service/src/main/java/org/tech/service/PrintLogService.java
@@ -0,0 +1,21 @@
+package org.tech.service;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * 测试打印错误日志
+ * @author Administrator
+ *
+ */
+@Service
+public class PrintLogService {
+	
+	public boolean echoErrorLog(boolean status){
+		if(status == false){
+			throw new NullPointerException();
+		}
+		return true;
+	}
+}
---
+++ tech-web/src/main/java/org/tech/controller/PrintLogController.java
@@ -0,0 +1,33 @@
+package org.tech.controller;
+
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.tech.service.PrintLogService;
+
+@Controller
+@RequestMapping(value="/error")
+public class PrintLogController extends BaseController{
+	protected final Log logger = LogFactory.getLog(getClass());
+	
+	@Resource
+	PrintLogService printLogService;
+	
+	@ResponseBody
+	@RequestMapping(value="/test1",method=RequestMethod.GET)
+	public Object test1(){
+		try {
+			printLogService.echoErrorLog(false);
+		} catch (Exception e) {
+			logger.error(e);
+		}
+		return responseSuccess("test",new Date());
+	}
+}
