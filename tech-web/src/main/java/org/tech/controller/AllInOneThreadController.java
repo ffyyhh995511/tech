2dba3e9e4cab2b5857304b20afcfee5374c6201c
ffyyhh995511 <ffyyhh995511@gmail.com>
2017-06-25 11:36:37 +0800

    当前容器的线程池大小测试 测试结果：jetty默认线程池大小200，tomcat也是200

--- tech-web/src/main/java/org/tech/controller/AllInOneThreadController.java
+++ tech-web/src/main/java/org/tech/controller/AllInOneThreadController.java
@@ -31,8 +31,9 @@ public class AllInOneThreadController extends BaseController{
 	@ResponseBody
 	@RequestMapping(value="/test1",method=RequestMethod.GET)
 	public Object test1(){
-		logger.info("service thread name:"+Thread.currentThread().getName());
-		logger.info("service thread id:"+Thread.currentThread().getId());
+		logger.info("controller thread name:"+Thread.currentThread().getName());
+		logger.info("controller thread id:"+Thread.currentThread().getId());
+		logger.info("controller activeCount:"+Thread.activeCount());
 		try {
 			People people = new People();
 			people.setId(UUIDUtil.getUUID());
@@ -43,4 +44,24 @@ public class AllInOneThreadController extends BaseController{
 		}
 		return responseSuccess("test one thread",new Date());
 	}
+	
+	/**
+	 * 当前容器的线程池大小测试
+	 * 
+	 * 测试结果：jetty默认线程池大小200，tomcat也是200
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value="/test2",method=RequestMethod.GET)
+	public Object test2(){
+		Thread ta[] = new Thread[Thread.activeCount()];   
+	    Thread.enumerate(ta);   
+	    System.out.println("Active thread's number:"+ta.length);   
+	    for(Thread t:ta){   
+	        if(t!=null)   
+	        System.out.println("Thread's name:"+t.getName()+" id:"+t.getId());  
+	    }
+	    return responseSuccess("test one thread",new Date());
+	}
+	 
 }
