e078745f0dbd28a01ca5c95e7057d01db17675be
方云鹤 <fangyunhe@mingbikes.com>
2017-06-28 16:41:31 +0800

    1:支持多文件上传 2:使用springMVC进行多文件的效率显然要比字符流写入方式效率上要高得多

--- tech-web/src/main/java/org/tech/controller/MultipleController.java
+++ tech-web/src/main/java/org/tech/controller/MultipleController.java
@@ -1,11 +1,21 @@
 package org.tech.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Iterator;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.tech.domain.People;
 import org.tech.service.MultipleService;
 
@@ -133,4 +143,47 @@ public class MultipleController extends BaseController{
 		}
 		return responseSuccess("success", null);
 	}
+	
+	/**
+	 * 1:支持多文件上传
+	 * 2:使用springMVC进行多文件的效率显然要比字符流写入方式效率上要高得多
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws IllegalStateException
+	 * @throws IOException
+	 */
+	@RequestMapping("/upload"  )  
+    public Object upload2(HttpServletRequest request,HttpServletResponse response) throws IllegalStateException, IOException {  
+        //创建一个通用的多部分解析器  
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());  
+        //判断 request 是否有文件上传,即多部分请求  
+        if(multipartResolver.isMultipart(request)){  
+            //转换成多部分request    
+            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest)request;  
+            //取得request中的所有文件名  
+            Iterator<String> iter = multiRequest.getFileNames();  
+            while(iter.hasNext()){  
+                //取得上传文件  
+                MultipartFile file = multiRequest.getFile(iter.next());  
+                if(file != null){  
+                    //取得当前上传文件的文件名称  
+                    String myFileName = file.getOriginalFilename();  
+                    //如果名称不为“”,说明该文件存在，否则说明该文件不存在  
+                    if(myFileName.trim() !=""){  
+                        System.out.println(myFileName);  
+                        //重命名上传后的文件名  
+                        String fileName = file.getOriginalFilename();  
+                        //定义上传路径  
+                        String path = request.getSession().getServletContext().getRealPath("/upload") + fileName;  
+                        File localFile = new File(path);  
+                        file.transferTo(localFile);  
+                    }  
+                }  
+            }  
+              
+        }  
+        return responseSuccess("test",new Date());
+    }
+	
 }
