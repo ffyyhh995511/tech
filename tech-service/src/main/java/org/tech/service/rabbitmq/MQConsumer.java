8f41fa775353fe9fdea122cb524deae271cb6476
方云鹤 <fangyunhe@mingbikes.com>
2017-07-05 16:20:56 +0800

    rabbitMQ添加ack对应的nack,在流量限制为只接受一个消息的情况下，没有返回ack和nack，线程会阻塞，同事nack支持把消息requeue还是直接丢弃

--- tech-service/src/main/java/org/tech/service/rabbitmq/MQConsumer.java
+++ tech-service/src/main/java/org/tech/service/rabbitmq/MQConsumer.java
@@ -1,12 +1,17 @@
 package org.tech.service.rabbitmq;
 
-import com.alibaba.fastjson.JSON;
-import com.rabbitmq.client.*;
-
 import java.io.IOException;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
+
+import com.alibaba.fastjson.JSON;
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
 
 /**
  * 消费者--异步处理消息
@@ -33,16 +38,23 @@ public class MQConsumer {
 					byte[] body) throws IOException {
 				String message = new String(body, "UTF-8");
 				Object gsmMessage = JSON.parseObject(message, Object.class);
-				log.info(" [Consumer] Received ");
-
-				//Map<String,String> map = LockHandler.processMsgID(gsmMessage);
+				log.info(" [Consumer] Received : "+ message);
 				Map<String,String> map = null;
 				//只有服务端返回非失败才发ack
-				if(!"2".equals(map.get("status"))){
+				if("1".equals(map.get("status"))){
+					// envelope.getDeliveryTag() 获取消息ID
+					// 接口返回消息除非网络异常和接口异常，其他（参数错误、已处理等）都做确认消息动作
 					// 返回接收到消息的确认信息
 					channel.basicAck(envelope.getDeliveryTag(), false);
+				}else if("2".equals(map.get("status"))){
+					// 如果nack的requeue = false,mq把消息直接丢弃
+					// message requeue = true
+					channel.basicNack(envelope.getDeliveryTag(), false, true);
+				}else if("3".equals(map.get("status"))){
+					// 参数不合法直接丢弃
+					channel.basicNack(envelope.getDeliveryTag(), false, false);
 				}
-				log.info(" [Consumer] Done");
+				log.info(" [Consumer] Done :" + message);
 				
 			}
 		};
