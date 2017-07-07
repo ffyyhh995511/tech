fa1ddd843075d27cfaf11436cdc40348836ec456
方云鹤 <fangyunhe@mingbikes.com>
2017-07-07 17:49:24 +0800

    @RunWith(SpringJUnit4ClassRunner.class) //配置了@ContextConfiguration注解并使用该注解的locations属性指明spring和配置文件之后， @ContextConfiguration(locations = {"classpath:spring.xml", "classpath:spring-mybatis.xml" })

---
+++ tech-web/src/test/java/springframe/test/PeopleController.java
@@ -0,0 +1,25 @@
+package springframe.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.tech.domain.People;
+import org.tech.service.PeopleService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+//配置了@ContextConfiguration注解并使用该注解的locations属性指明spring和配置文件之后，
+@ContextConfiguration(locations = {"classpath:spring.xml", "classpath:spring-mybatis.xml" })
+public class PeopleController {
+	
+	 @Autowired
+	 private PeopleService peopleService;
+	 
+	 @Test
+	 public void test(){
+		 People people = new People();
+		 people.setName("t");
+		 System.out.println(peopleService.add(people));
+	 }
+}
